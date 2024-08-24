ebd7f48e62d26ec97e1a651c1ae5d4be1ed0ff5e
Naufal Wiwit Putra <naufalwiwitputra@outlook.com>
2024-08-24 18:17:02 +1000

    db: drizzle migrate init

--- src/app/db/index.ts
+++ src/app/db/index.ts
@@ -2,8 +2,10 @@ import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 config({ path: '.env' }); // or .env.local
+import * as schema from "./schema"
+
 const client = createClient({
     url: process.env.DATABASE_CONNECTION_URL!,
     authToken: process.env.DATABASE_AUTH_TOKEN!,
 });
-export const db = drizzle(client);
+export const db = drizzle(client, { schema });
