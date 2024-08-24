4811ce660acb64ba2f97dc452ff434a90ac8d807
Naufal Wiwit Putra <naufalwiwitputra@outlook.com>
2024-08-24 22:43:57 +1000

    nooo

--- src/app/clips/get/route.ts
+++ src/app/clips/get/route.ts
@@ -2,10 +2,7 @@ export async function GET() {
 
     const videos = [
         "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
-        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
         "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
     ];
     return Response.json({ videos: [videos[Math.floor(Math.random()*4)]] })
 }
