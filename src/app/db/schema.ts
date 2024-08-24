ecd79501ef9928a7d78ba4701dc003a139c04d59
Naufal Wiwit Putra <naufalwiwitputra@outlook.com>
2024-08-24 18:53:34 +1000

    db: define created at

--- src/app/db/schema.ts
+++ src/app/db/schema.ts
@@ -1,10 +1,9 @@
-import { sql } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const events = sqliteTable('event', {
     id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
     organizer: text('organizer').default("Anonymous"),
-    created_at: integer('id', { mode: 'timestamp_ms' }).defaultNow(),
+    created_at: integer('created_at', { mode: 'timestamp_ms' }).defaultNow(),
     start_at: integer('start_at', { mode: 'timestamp_ms' }).notNull(),
     end_at: integer('end_at', { mode: 'timestamp_ms' }).notNull(),
     lat: integer('lat').default(0),
